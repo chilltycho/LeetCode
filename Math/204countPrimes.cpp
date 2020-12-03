44d82417c01d8526a937bb1177450058acf28399
chilltycho <chilltycho@gmail.com>
2020-12-03 19:07:09 +0800

    update

--- Math/204countPrimes.cpp
+++ Math/204countPrimes.cpp
@@ -19,12 +19,10 @@ int countPrimes(int n)
             count++;
     return count;
 }
-
+//埃氏筛，如果x是质数，则2x,3x..一定不是质数。优化，直接从x*x开始标记，因为2x,3x..一定在x之前被其他数的倍数标记过了
 int countPrimes_Sieve(int n)
 {
-    vector<bool> isPrime;
-    for (int i = 0; i < n; i++)
-        isPrime.push_back(true);
+    vector<bool> isPrime(n, true); //默认全是质数
     for (int i = 2; i <= sqrt(n); i++)
         if (isPrime[i])
             for (int j = i * i; j < n; j += i)
@@ -35,8 +33,27 @@ int countPrimes_Sieve(int n)
             count++;
     return count;
 }
-
+//线性筛,让每个合数只标记一次。45同时被3,5两个数标记
+int countPrimes_3(int n)
+{
+    vector<int> primes;
+    vector<int> isPrime(n, 1);
+    for (int i = 2; i < n; i++)
+    {
+        if (isPrime[i])
+            primes.push_back(i);
+        for (int j = 0; j < primes.size() && i * primes[j] < n; j++)
+        {
+            isPrime[i * primes[j]] = 0;
+            if (i % primes[j] == 0) //对合数y=x*primes[j],一定在遍历到x/primes[j]*primes[j]时被标记
+            {
+                break;
+            }
+        }
+    }
+    return primes.size();
+}
 int main()
 {
-    cout << countPrimes_Sieve(100) << endl;
+    cout << countPrimes_3(100) << endl;
 }
