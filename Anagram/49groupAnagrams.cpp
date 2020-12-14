b10ffcdcb0e362d9fd152b768d41e19c3d89eea4
chilltycho <chilltycho@gmail.com>
2020-12-14 20:06:25 +0800

    12_14

---
+++ Anagram/49groupAnagrams.cpp
@@ -0,0 +1,42 @@
+/*给定字符串数组，将字母异位词组合在一起*/
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string> &strs)
+{
+    vector<vector<string>> res;
+    unordered_map<string, size_t> us;
+    if (strs.empty())
+        return res;
+    int i = 1;
+    for (auto &s : strs)
+    {
+        string temp = s;
+        sort(temp.begin(), temp.end());
+        if (us.count(temp))
+        {
+            res[us[temp] - 1].push_back(s);
+        }
+        else
+        {
+            us[temp] = i++;
+            res.push_back(vector<string>{s});
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    vector<string> vs{"eat", "tea", "tan", "ate", "nat", "bat"};
+    auto res = groupAnagrams(vs);
+    for (auto c : res)
+    {
+        for (auto cc : c)
+            cout << cc << ' ';
+        cout << endl;
+    }
+}
---
+++ BinaryTree/95generateTrees.cpp
@@ -0,0 +1,44 @@
+/*给定整数n，生成所有由1..n为节点组成的二叉搜索树*/
+#include "TreeNode.h"
+using namespace std;
+
+vector<TreeNode *> helper(int start, int end)
+{
+    vector<TreeNode *> re;
+    if (start > end)
+    {
+        //若左子树为空，右子树不为空，要正确构建所有树，需对左右子树列表遍历
+        re.push_back(nullptr);
+        return re;
+    }
+    for (int i = start; i <= end; i++)
+    {
+        //TreeNode *root=new TreeNode(i);以当前root根节点的数个数num=left.size()*right.size()>1时，num颗子树共用root节点
+        //获取所有可行左子树集合
+        vector<TreeNode *> left = helper(start, i - 1);
+        //获取所有可行右子树集合
+        vector<TreeNode *> right = helper(i + 1, end);
+        //从左子树集合选出一颗右子树，从右子树集合选出一颗右子树，拼接到根节点
+        for (TreeNode *l : left)
+        {
+            for (TreeNode *r : right)
+            {
+                TreeNode *root = new TreeNode(i);
+                root->left = l;
+                root->right = r;
+                re.push_back(root);
+            }
+        }
+    }
+    return re;
+}
+vector<TreeNode *> generateTrees(int n)
+{
+    if (n < 1)
+        return vector<TreeNode *>{};
+    return helper(1, n);
+}
+
+int main()
+{
+}
