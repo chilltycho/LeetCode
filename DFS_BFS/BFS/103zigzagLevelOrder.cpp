05fe28670ae5ae4427e77685a70cccb7e19d5a69
chilltycho <chilltycho@gmail.com>
2020-12-22 19:14:04 +0800

    12_22

--- DFS_BFS/BFS/103zigzagLevelOrder.cpp
+++ DFS_BFS/BFS/103zigzagLevelOrder.cpp
@@ -4,53 +4,64 @@ using namespace std;
 //层序，第一层左向右，第二层右向左...
 vector<vector<int>> zigzagLevelOrder(TreeNode *root)
 {
-    deque<TreeNode *> mq;
-    vector<vector<int>> res;
+    vector<vector<int>> ans;
     if (root == nullptr)
-        return res;
+        return ans;
+    queue<TreeNode *> nodeQueue;
+    nodeQueue.push(root);
     bool isleft = true;
-    mq.push_back(root);
-    while (!mq.empty())
+    while (!nodeQueue.empty())
     {
-        int size = mq.size();
-        vector<int> temp;
-        if (isleft)
+        deque<int> levelList;
+        int size = nodeQueue.size();
+        for (int i = 0; i < size; i++)
         {
-            for (int i = 0; i < size; i++)
-            {
-                auto node = mq.front();
-                mq.pop_front();
-                temp.push_back(node->val);
-                if (node->left)
-                    mq.push_back(node->left);
-                if (node->right)
-                    mq.push_back(node->right);
-            }
+            auto node = nodeQueue.front();
+            nodeQueue.pop();
+            if (isleft)
+                levelList.push_back(node->val);
+            else
+                levelList.push_front(node->val);
+            if (node->left)
+                nodeQueue.push(node->left);
+            if (node->right)
+                nodeQueue.push(node->right);
         }
-        else
-        {
-            for (int i = 0; i < size; i++)
-            {
-                auto node = mq.back();
-                mq.pop_back();
-                temp.push_back(node->val);
-                if (node->right)
-                    mq.push_front(node->right);
-                if (node->left)
-                    mq.push_front(node->left);
-            }
-        }
-        res.push_back(temp);
+        ans.emplace_back(vector<int>{levelList.begin(), levelList.end()});
         isleft = !isleft;
     }
+    return ans;
+}
+
+void dfs(TreeNode *cur, vector<deque<int>> &res, int level)
+{
+    if (cur == nullptr)
+        return;
+    if (res.size() <= level)
+        res.push_back(deque<int>{});
+    if (level % 2 == 0)
+        res[level].push_back(cur->val);
+    else
+        res[level].push_front(cur->val);
+    dfs(cur->left, res, level + 1);
+    dfs(cur->right, res, level + 1);
+}
+
+vector<vector<int>> zigzagLevelOrder_dfs(TreeNode *root)
+{
+    vector<vector<int>> res;
+    vector<deque<int>> vd;
+    dfs(root, vd, 0);
+    for (auto &dq : vd)
+        res.emplace_back(vector<int>{dq.begin(), dq.end()});
     return res;
 }
 
 int main()
 {
     vector<int> vi{3, 9, 20, -1, -1, 15, 7};
-    auto root=vecToTree(vi);
-    auto vii = zigzagLevelOrder(root);
+    auto root = vecToTree(vi);
+    auto vii = zigzagLevelOrder_dfs(root);
     for (auto c : vii)
     {
         for (auto cc : c)
