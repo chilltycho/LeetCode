fe0c28c5226735c55d3d49eb41a10b9f2263e06b
chilltycho <chilltycho@gmail.com>
2020-12-26 21:03:38 +0800

    12_26

---
+++ DFS_BFS/126findLadders.cpp
@@ -0,0 +1,102 @@
+/*给定两个单词begin,end，和字典，找出所有从begin
+到end的最短转换序列。每次转换只能改变一个字母。
+转换后单词必须是字典中单词。*/
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+/**
+ * 将每个单词抽象为点，建立图模型。给每个单词标号。同时创建id到word映射
+ * cost数组，cost[i]表示beginword到第i个点代价。
+*/
+unordered_map<string, int> wordId;
+vector<string> idWord;
+vector<vector<int>> edges;
+
+vector<vector<string>> findLadders(string beginWord, string endWord, vector<string> &wordList)
+{
+    int id = 0;
+    for (auto word : wordList)
+    {
+        if (!wordId.count(word))
+        {
+            wordId[word] = id++;
+            idWord.push_back(word);
+        }
+    }
+    if (!wordId.count(endWord))
+        return {};
+
+    if (!wordId.count(beginWord))
+    {
+        wordId[beginWord] = id++;
+        idWord.push_back(beginWord);
+    }
+
+    //建立图
+    edges.resize(idWord.size());
+    for (int i = 0; i < idWord.size(); i++)
+    {
+        for (int j = i + 1; j < idWord.size(); j++)
+        {
+            if (transformCheck(idWord[i], idWord[j]))
+            {
+                edges[i].push_back(j);
+                edges[j].push_back(i);
+            }
+        }
+    }
+
+    //目标id
+    int dest = wordId[endWord];
+    vector<vector<string>> res;
+    queue<vector<int>> q;
+    vector<int> cost(id, INT_MAX);
+    //起点加入队列，cost设置为0
+    q.push(vector<int>{wordId[beginWord]});
+    cost[wordId[beginWord]] = 0;
+
+    //开始广度搜索
+    while (!q.empty())
+    {
+        vector<int> now = q.front();
+        q.pop();
+        int last = now.back();
+        //遇到终点
+        if (last == dest)
+        {
+            vector<string> tmp;
+            for (auto i : now)
+                tmp.push_back(idWord[i]);
+            res.push_back(tmp);
+        }
+        else
+        {
+            for (int i = 0; i < edges[last].size(); i++)
+            {
+                int to = edges[last][i];
+                if (cost[last] + 1 <= cost[to])
+                {
+                    cost[to] = cost[last] + 1;
+                    vector<int> tmp(now);
+                    tmp.push_back(to);
+                    q.push(tmp);
+                }
+            }
+        }
+    }
+    return res;
+}
+
+bool transformCheck(string &s1, string &s2)
+{
+    int differences = 0;
+    for (int i = 0; i < s1.size() && differences < 2; i++)
+    {
+        if (s1[i] != s2[i])
+            ++differences;
+    }
+    return differences == 1;
+}
---
+++ DynamicProgramming/120minimumTotal.cpp
@@ -0,0 +1,21 @@
+/*给定杨辉三角形，找出自顶向下最小路径和。
+每步只能向下移动到下标相同或下标+1的节点*/
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+//f[i][j]=min(f[i-1][j-1],f[i-1][j])+c[i][j]
+int minimumTotal(vector<vector<int>>& traingle)
+{
+    int n=traingle.size();
+    vector<vector<int>> dp(n,vector<int>(n));
+    dp[0][0]=traingle[0][0];
+    for(int i=1;i<n;i++)
+    {
+        dp[i][0]=dp[i-1][0]+traingle[i][0];
+        for(int j=1;j<i;j++)
+            dp[i][j]=min(dp[i-1][j-1],dp[i-1][j])+traingle[i][j];
+        dp[i][i]=dp[i-1][i-1]+traingle[i][i];
+    }
+    return *min_element(dp[n-1].begin(),dp[n-1].end());
+}
