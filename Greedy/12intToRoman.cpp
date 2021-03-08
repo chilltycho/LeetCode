e8f4f253d78c258e5c485f03e0046c32e808ea7f
chilltycho <chilltycho@gmail.com>
2021-03-08 22:56:54 +0800

    update

--- Greedy/12intToRoman.cpp
+++ Greedy/12intToRoman.cpp
@@ -3,6 +3,10 @@ IV(4) IX(9) XL(40) XC(90) CD(400) CM(900)*/
 #include <iostream>
 #include <cassert>
 using namespace std;
+/**
+ * 尽可能使用较大数值对应的字符，最后转换的罗马数字的字符个数更少。
+ * 
+*/
 string intToRoman(int num)
 {
     int nums[13]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
