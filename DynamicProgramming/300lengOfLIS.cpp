d27039b16fe7d94e6c8ba208e2bf3fc2637cb216
chilltycho <chilltycho@gmail.com>
2021-03-30 13:34:20 +0800

    update

--- DynamicProgramming/300lengOfLIS.cpp
+++ DynamicProgramming/300lengOfLIS.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 /*
 最长上升子序列问题：给定一个无序的整数数组，找到其中最长上升子序列的长度。
@@ -11,23 +11,23 @@ using namespace std;
 1.明确dp[i]意义，dp[i]表示以nums[i]这个数结尾的最长递增子序列长度。
 2.若nums[i]>nums[i-1]，则dp[i]=dp[i-1]+1。否则比较dp[i-2]直到nums[i-k]>nums[i]或i-k<0为止
 */
-int lengofLIS(vector<int>& nums)
+int lengofLIS(vector<int> &nums)
 {
-    vector<int> dp(nums.size(),1);//初始化为1，因为对每个元素最短上升子序列为其本身，也就是1，注意dp数组长度
-    for(int i=0;i<nums.size();i++)
+    vector<int> dp(nums.size(), 1); //初始化为1，因为对每个元素最短上升子序列为其本身，也就是1，注意dp数组长度
+    for (int i = 0; i < nums.size(); i++)
     {
-        for(int j=0;j<i;j++)
-            if(nums[i]>nums[j])
-                dp[i]=max(dp[i],dp[j]+1);
+        for (int j = 0; j < i; j++)
+            if (nums[i] > nums[j])
+                dp[i] = max(dp[i], dp[j] + 1);
     }
-    int res=0;
-    for(int i=0;i<dp.size();i++)
-        res=max(res,dp[i]);
+    int res = 0;
+    for (int i = 0; i < dp.size(); i++)
+        res = max(res, dp[i]);
     return res;
 }
 
 int main()
 {
-    vector<int> a{10,9,2,5,3,7,101,18};
-    cout<<lengofLIS(a);
+    vector<int> a{10, 9, 2, 5, 3, 7, 101, 18};
+    cout << lengofLIS(a);
 }
