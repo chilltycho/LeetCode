0c1f2c1d0c9fcdab979b7e0ee1432f937bd9ea03
chilltycho <chilltycho@gmail.com>
2021-07-08 08:15:24 +0800

    update

--- BinaryTree/TreeNode.h
+++ BinaryTree/TreeNode.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <queue>
 #include <stack>
+#include <algorithm>
 using std::cout;
 using std::queue;
 using std::stack;
@@ -52,4 +53,66 @@ TreeNode *vecToTree(vector<int> &nums)
         i++;
     }
     return root;
+}
+
+vector<int> inorder(TreeNode *root)
+{
+    vector<int> res;
+    stack<TreeNode *> st;
+    auto rt = root;
+    while (rt || st.size())
+    {
+        if (rt)
+        {
+            st.push(rt);
+            rt = rt->left;
+        }
+        else
+        {
+            auto node = st.top();
+            st.pop();
+            res.push_back(node->val);
+            rt = node->right;
+        }
+    }
+    return res;
+}
+
+vector<int> preorder(TreeNode *root)
+{
+    vector<int> res;
+    stack<TreeNode *> st;
+    auto rt = root;
+    while (rt || st.size())
+    {
+        while (rt)
+        {
+            res.push_back(rt->val); // 一直向左走，把当前值记录，把右节点入栈
+            st.push(rt->right);
+            rt = rt->left;
+        }
+        rt = st.top();
+        st.pop();
+    }
+    return res;
+}
+
+vector<int> postorder(TreeNode *root)
+{
+    stack<TreeNode *> stk;
+    auto rt = root;
+    vector<int> res;
+    while (rt || stk.size())
+    {
+        while (rt)
+        {
+            res.push_back(rt->val);
+            stk.push(rt->left);
+            rt = rt->right;
+        }
+        rt = stk.top();
+        stk.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res;
 }
--- BinaryTree/testTreeNode.cpp
+++ BinaryTree/testTreeNode.cpp
@@ -5,5 +5,7 @@ int main()
 {
     vector<int> vi{1, 2, 3, -1, 4, 5, 6};
     auto t = vecToTree(vi);
-    inOrder_ite(t);
+    auto res = inorder(t);
+    for (auto c : res)
+        cout << c << ' ';
 }
