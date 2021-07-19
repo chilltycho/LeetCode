b975216eb168ca1905071bc86c7ddf805ab92075
lichang08 <lichang08@MacBook-Pro.local>
2021-07-19 10:28:20 +0800

    update

--- Sort/heapsort.cpp
+++ Sort/heapsort.cpp
@@ -6,13 +6,13 @@ void heapify(vector<int> &arr, int n, int i)
 {
     // 最大元素为i
     auto largest = i;
-    auto l = 2 * i + 1;
-    auto r = 2 * i + 2;
+    auto l = 2 * i + 1; // 左子节点
+    auto r = 2 * i + 2; // 右子节点
     if (l < n && arr[l] > arr[largest])
         largest = l;
     if (r < n && arr[r] > arr[largest])
         largest = r;
-    if (largest != i)
+    if (largest != i) // 根节点不是最大
     {
         swap(arr[largest], arr[i]);
         heapify(arr, n, largest);
@@ -21,9 +21,13 @@ void heapify(vector<int> &arr, int n, int i)
 
 void heapsort(vector<int> &arr, int n)
 {
-    for (int i = n / 2 - 1; i >= 0; --i) // 建立堆，对所有父节点，保证比子节点小
+    for (int i = n / 2 - 1; i >= 0; --i) // 建立堆，对所有父节点，保证比子节点小，最大堆
         heapify(arr, n, i);              // O(n)次调用heapify, heapify:O(log(n)
-    for (int i = n - 1; i > 0; --i)      // 弹出根节点
+    cout << "堆：" << endl;
+    for (auto c : arr)
+        cout << c << ' ';
+    cout << endl;
+    for (int i = n - 1; i > 0; --i) // 弹出根节点
     {
         swap(arr[0], arr[i]);
         heapify(arr, i, 0);
