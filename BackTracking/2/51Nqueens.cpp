5917477cadbcfa9078f505ce03ffcb838a6b5e81
chilltycho <chilltycho@gmail.com>
2023-08-07 20:06:40 +0800

    update

--- BackTracking/2/51Nqueens.cpp
+++ BackTracking/2/51Nqueens.cpp
@@ -7,28 +7,25 @@ vector<vector<string>> res;
 bool isValid(vector<string> &board, int row, int col) {
   int n = board.size();
   // 同列
-  for (int i = 0; i < n; i++) 
-  {
+  for (int i = 0; i < n; i++) {
     if (board[i][col] == 'Q')
       return false;
   }
   // 右上
-  for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) 
-  {
+  for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
     if (board[i][j] == 'Q')
       return false;
   }
   // 左上
-  for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) 
-  {
+  for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
     if (board[i][j] == 'Q')
       return false;
   }
   return true;
 }
 
-void dfs(vector<string> &board, int row) // 考虑每行
-{
+void dfs(vector<string> &board, int row) {
+  // 考虑每行
   if (row == board.size()) {
     res.push_back(board);
     return;
